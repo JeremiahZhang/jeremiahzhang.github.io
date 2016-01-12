e574ca7b14d27d05633d8b2e404add237aa78c0c
JeremiahZhang <zhangleisuda@gmail.com>
2016-01-12 10:24:51 +0800

    shuffle setup

--- fabfile.py
+++ fabfile.py
@@ -2,12 +2,15 @@
 
 from fabric.api import local
 
+# fab post: your-post-name
 def post(name):
     local("./scripts/newpost %s" % name)
 
+# fab tag
 def tag():
     local("./scripts/generate-tags")
 
+# please use: fab pu:'discribation'
 def pu(discribation):
     local("git ad")
     local("git ci -m '%s' " % discribation)
